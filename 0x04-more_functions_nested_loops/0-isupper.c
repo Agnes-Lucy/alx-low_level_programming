55618bf907e44d6278d453b2d5a4f9ba45d6e5b7
Agnes Udoh <udohlucee@yahoo.com>
2023-03-24 18:19:40 -0700

    Task zero

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -7,12 +7,14 @@
  * Otherwise, we return 0
  */
 
-int _isuper(int c)
+int _isupper(int c)
 {
 	int start = 65, end = 90;
 
-	if (c > = start && c < = end)
+	if (c >= start && c <= end)
+	{
 		return (1);
+	}
 	return (0);
 }
 
