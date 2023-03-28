f3addf7f787b6436623f18b58c5d461077c8a58e
Agnes Udoh <udohlucee@yahoo.com>
2023-03-28 09:06:48 -0700

    Task seven

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,18 +2,26 @@
 
 /**
  * puts2 - print strings
- * @str: function parameter
+ * @str: string to be printed
  * Return: 0
  */
 
 void puts2(char *str)
 {
 	int i;
+	int count = 0;
 
-	for (i = 0; str[i] != '\0'; i++)
+	while (*str)
 	{
-		_putchar(str[i]);
-		i++;
+		count++;
+		str++;
+	}
+	for (i = 0; i < count; i++)
+		str--;
+	for (i = 0; i < count; i++)
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
 	}
 	_putchar('\n');
 }
