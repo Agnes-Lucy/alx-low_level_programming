08489ed996870a4d0ebf2b6ef1b0dae0f335dae0
Agnes Udoh <udohlucee@yahoo.com>
2023-04-11 11:40:08 -0700

    Task zero

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Prints the name of the prograM
+ * @argc: Argument count
+ * @argy: Argument vector
+ * Return: 0
+ */
+
+int main(__attribute__((unused))int argc, char *argy[])
+{
+	printf ("%s\n", argy[0]);
+	return (0);
+}
